0b652908f144f3754489f4fd8779f7d84d6a5d55
rizki-arvi <rizkiarvi7@gmail.com>
2026-08-17 12:17:33 +0700

    Update hapus.php

--- service/hapus.php
+++ service/hapus.php
@@ -1,8 +1,5 @@
 <?php
-// service/hapus.php
 
 include "database.php";
 
-// Fitur hapus belum selesai dikerjakan, jadi datanya belum benar-benar
-// dihapus dari database. Sementara balik ke daftar buku dulu.
 header("Location: ../index.php");
