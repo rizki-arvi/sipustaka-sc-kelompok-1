dda58f95d4e416d271e9109a0a5ee117334e3dbc
rizki-arvi <rizkiarvi7@gmail.com>
2026-08-17 12:17:54 +0700

    Update update.php

--- service/update.php
+++ service/update.php
@@ -1,8 +1,6 @@
 <?php
-// service/update.php
 
 include "database.php";
 
-// Fitur edit belum selesai dikerjakan, jadi datanya belum benar-benar
-// diupdate ke database. Sementara balik ke daftar buku dulu.
+
 header("Location: ../index.php");
